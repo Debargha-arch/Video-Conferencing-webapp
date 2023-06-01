721703258e32c7359d910423fe6a034fcb5bd3ea
Debargha-arch <mukherjeedebargha281@gmail.com>
2023-06-01 19:22:01 +0530

    express server added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
-const { Server } = require('socket.io');
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
 
-const port = process.env.PORT || 8000;
-
-const io = new Server(port, {
-    cors: true
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
 });
+app.use(cors());
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+	res.send('Server is running...');
+});
+
+// const { Server } = require('socket.io');
+
+// const port = process.env.PORT || 8000;
+
+// const io = new Server(port, {
+//     cors: true
+// });
 
 const emailToSocketIdMap = new Map();
 const socketIdToEmailMap = new Map();
@@ -39,3 +57,5 @@ io.on("connection", (socket) => {
         io.to(to).emit("peer:nego:final", {from: socket.id, ans});
     });
 })
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
